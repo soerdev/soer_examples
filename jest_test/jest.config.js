7d02d5eeda5fccc9d9bf06d33e42f0920d4c1094
Evgeniy Sergeev <esergeev777@yandex.ru>
2022-06-22 06:46:12 +0300

    chore: add reporter

--- jest_test/jest.config.js
+++ jest_test/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	reporters: ['jest-dot-reporter'],
+	reporters: ['./soer_reporter.js'],
 	globals: {
 		'ts-jest': {
 			diagnostics: {

c33e96315da5886859cdebc000eada1420891136
Evgeniy Sergeev <esergeev777@yandex.ru>
2022-06-30 13:36:55 +0300

    chore: add deep merge example

--- jest_test/deep_merge.ts
+++ jest_test/deep_merge.ts
@@ -1,33 +1,36 @@
 function isObject(o: any): o is object {
 	return typeof(o) === 'object' && !Array.isArray(o);
 }
-
 function mergeValues(a: any, b: any): any {
+
 	if (isObject(a) && isObject(b)) {
 		return deepMerge(a, b);
-	}
-	if (Array.isArray(a) && !Array.isArray(b)) {
-		return [...a, b];
-	}
-	if (!Array.isArray(a) && Array.isArray(b)) {
-		return [a, ...b];
 	}
 	if (Array.isArray(a) && Array.isArray(b)) {
 		return [...a, ...b];
 	}
-	return b || a;
+	if (!Array.isArray(a) && Array.isArray(b)) {
+		return [a, ...b];
+	}
+	if (Array.isArray(a) && !Array.isArray(b)) {
+		return [...a, b];
+	}
+			
+	return b === undefined ? a : b;
 }
 
+export default function deepMerge(
+	source: object,
+	target: object) : object {
+		const result = {};
+		const okeys = new Set();
+		Object.keys(source).forEach(okeys.add, okeys);
+		Object.keys(target).forEach(okeys.add, okeys);
 
-export default function deepMerge(source: object, target: object): object {
-	const result = {};
-	const okeys = new Set();
-	Object.keys(source).forEach(okeys.add, okeys);
-	Object.keys(target).forEach(okeys.add, okeys);
-
-	okeys.forEach((key: string) => {
-		result[key] = mergeValues(source[key], target[key]);
-	});
-	return result;
-
-}
+		okeys.forEach((key: string) => 
+			result[key] = mergeValues(
+				source[key],
+				target[key])
+		);
+		return result;
+	}
---
+++ jest_test/tests/deep_merge.test.ts
@@ -0,0 +1,24 @@
+import deepMerge from '../deep_merge';
+
+describe('Deep merge', () => {
+
+	it ('should merge simple object with uniq keys', () => {
+		expect(deepMerge({a: 1}, {b: 1})).toEqual({a: 1, b: 1});
+	});
+
+	it ('should merge simple object without uniq keys', () => {
+		expect(deepMerge({a: 1}, {a: 2})).toEqual({a: 2});
+		expect(deepMerge({a: [1]}, {a: [2]})).toEqual({a: [1, 2]});
+		expect(deepMerge({a: 1}, {a: [2]})).toEqual({a: [1, 2]});
+		expect(deepMerge({a: [1]}, {a: 2})).toEqual({a: [1, 2]});
+	});
+
+	it ('should merge nested object', () => {
+		expect(deepMerge({a: {b: 1}}, {a: {b: 2}})).toEqual({a: {b: 2}}); 
+		expect(deepMerge({a: {b: [1]}}, {a: {b: 2}})).toEqual({a: {b: [1, 2]}}); 
+
+
+	});
+
+});
+
